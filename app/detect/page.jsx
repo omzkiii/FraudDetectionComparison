7ab14a1fc52c7767a8336cc1d348f8c8011758d1
ron <ronespinosa999@gmail.com>
2024-06-28 16:36:55 +0800

    add ui's

--- app/detect/page.jsx
+++ app/detect/page.jsx
@@ -1,5 +1,7 @@
 "use client"
 import React, { useState } from 'react'
+import CardDetect from '../components/CardDetect'
+
 
 function Testing() {
   const [textData, setTextData] = useState('');
@@ -19,16 +21,12 @@ function Testing() {
     setTextData(textData);
   };
     return (
-      <div>
-        HELLO WORLD!
-        <form onSubmit={handleSubmit} className="p-6 rounded shadow-md">
-          <h1> SMS Fraud Detection </h1>
-          <textarea className="focus:outline-none focus:ring-2 focus:ring-indigo-600"
-            onChange={(e) => setTextData(e.target.value)}>
-          </textarea>
-          <button type='submit'>Submit</button>
-        </form>
-      </div>
+      <>
+        <div className="min-h-screen bg-white flex flex-col justify-center items-center">
+        <h1 className="text-5xl font-bold mb-8">Fraud Detection</h1>
+        <CardDetect />
+        </div>
+    </>
     )
   }
   
