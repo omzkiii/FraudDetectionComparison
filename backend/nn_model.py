28f310db4f9ddb6fc29923a4fd5ef17242a1723b
omzkiii <geogeomarsantos@gmail.com>
2024-07-01 15:46:47 +0800

    update utils

--- backend/nn_model.py
+++ backend/nn_model.py
@@ -1,30 +1,34 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import sklearn
 from sklearn.neural_network import MLPClassifier
-from sklearn.neural_network import MLPRegressor
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from math import sqrt
-from sklearn.metrics import r2_score
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+
 
 from sklearn.metrics import classification_report, confusion_matrix
 
-X_train = []
-X_test = []
-y_train = []
-y_test = []
+import utils
 
-mlp = MLPClassifier(hidden_layer_sizes=(500,), activation='relu', solver='adam', max_iter=500)
-mlp.fit(X_train, y_train)
 
-predict_train = mlp.predict(X_train)
-predict_test = mlp.predict(X_test)
+def neural_network(): 
+    X_train, X_test, y_train, y_test = utils.get_training_data(utils.preprocess(), 0.3, 2)
 
-cm_train = confusion_matrix(y_train, predict_train)
-cr_train = classification_report(y_train, predict_train)
+    mlp = MLPClassifier(hidden_layer_sizes=(500,), activation='relu', solver='adam', max_iter=500)
+    mlp.fit(X_train, y_train)
 
-cm_test = confusion_matrix(y_test, predict_test)
-cr_test = classification_report(y_test, predict_test)
+    predict_train = mlp.predict(X_train)
+    predict_test = mlp.predict(X_test)
+
+    # cm_train = confusion_matrix(y_train, predict_train)
+    # cr_train = classification_report(y_train, predict_train)
+
+    # cm_test = confusion_matrix(y_test, predict_test)
+    # cr_test = classification_report(y_test, predict_test)
+
+    accuracy = accuracy_score(y_test, predict_test)
+    recall = recall_score(y_test, predict_test)
+    precision = precision_score(y_test, predict_test)
+    f1 = f1_score(y_test, predict_test)
+    
+    return
+
+
+
